ea6b2561d8fa4585898461d10c9f18a8ca55f487
bilalkhan123456789 <bk315818@gmail.com>
2023-10-31 18:38:58 +0500

    second

--- src/Components/Flights.jsx
+++ src/Components/Flights.jsx
@@ -1,20 +1,20 @@
 import React, { useState } from "react";
-import booking from "../assests/booking.png";
-import download from "../assests/download.png";
-import help from "../assests/help.png";
-import haram from "../assests/Images/haram.png";
-import masjid from "../assests/Images/masjid.png";
-import world from "../assests/Images/world.png";
-import plus from "../assests/plus.png";
-import minus from "../assests/minus.png";
+// import booking from "../assests/booking.png";
+// import download from "../assests/download.png";
+// import help from "../assests/help.png";
+// import haram from "../assests/Images/haram.png";
+// import masjid from "../assests/Images/masjid.png";
+// import world from "../assests/Images/world.png";
+// import plus from "../assests/plus.png";
+// import minus from "../assests/minus.png";
 
-import umrahvisa from "../assests/buttonIcons/umrahvisa.png";
+// import umrahvisa from "../assests/buttonIcons/umrahvisa.png";
 
-import madina from "../assests/buttonIcons/madina.png";
-import mecca from "../assests/buttonIcons/mecca.png";
-import dateoftravel from "../assests/buttonIcons/dateoftravel.png";
-import returndate from "../assests/buttonIcons/returndate.png";
-import passengers from "../assests/buttonIcons/passengers.png";
+// import madina from "../assests/buttonIcons/madina.png";
+// import mecca from "../assests/buttonIcons/mecca.png";
+// import dateoftravel from "../assests/buttonIcons/dateoftravel.png";
+// import returndate from "../assests/buttonIcons/returndate.png";
+// import passengers from "../assests/buttonIcons/passengers.png";
 
 
 
@@ -25,15 +25,16 @@ function Flight() {
   return (
     <>
     
-    <div style={{marginTop:-580+"px"}}  className="w-4/5 float-right  ">
+    <div className="w-4/5 float-right  flex mt-[-580px] bg-red-700 ">
         
-       
-
-         
-    <div className="flex justify-center items-center">
       
-<h1 className="text-4xl">Processing</h1>       
-      </div>    
+
+<div className="w-1/5 bg-gray-100 h-96 "><h1>hellooo</h1></div>
+
+
+<div className="w-4/5 bg-blue-500">  <h1>htruufjjhg</h1></div>
+
+
           
           
 
